25befe1deb9f74f7c7b91710718dd6f380d9a6f4
Ubaidullah <ubaidsofi641@gmail.com>
2026-08-10 21:51:09 +0530

    initial commit

---
+++ 2nd program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age =18;
+    float marks = 85.6;
+    char grade = 'A';
+    
+    printf("my age is %d\n", age);
+    printf("my marks are %f\n", marks);
+    printf("my grade is %C\n", grade);
+
+    return 0;
+
+}
---
+++ main.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+ 
+int main(){
+   
+  //this is a first program@@ :) 
+   
+  /* this 
+  is 
+  also 
+  a 
+  comment */ 
+   
+    printf("I Love Food!\n");
+    printf("its really great");
+
+ return 0;
+}
+
