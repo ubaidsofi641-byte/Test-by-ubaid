b5d1c05fa0f5f2f82ff224279ea050a8e57c4eeb
ubaidsofi641-byte <ubaidsofi641@gmail.com>
2026-08-10 23:38:11 +0530

    Add files via upload

---
+++ 3rd_program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    double pi = 3.14159612131;
+    double e = 2.71818182845;
+
+    
+    printf("the value of pi is %.10lf\n", pi);
+    printf("the value of e is %.10lf\n", e);
+
+
+    return 0;
+
+}
---
+++ 4th_program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+char symbol = '!';
+char currency = '$';
+
+
+printf("Oh my Lord %c\n" , symbol);
+printf("I bought a shoe for 20%C\n" , currency);
+
+
+
+
+    
+
+    return 0;
+}
